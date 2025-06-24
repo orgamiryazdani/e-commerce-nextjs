d8f4bd7cd9e74336d27e8cf79b1af9a0b36d49c7
orgamiryazdani <orgamiryazdani@gmail.com>
2025-06-24 20:54:38 +0330

    chore(security): add Content-Security-Policy header in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
-
 const withNextIntl = createNextIntlPlugin();
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+const nextConfig: NextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+        ]
+    },
+};
+
 export default withNextIntl(nextConfig);
