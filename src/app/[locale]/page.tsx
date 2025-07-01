cc31e8a9dfe0a7a9aaed2162cf3ba380e84677a8
orgamiryazdani <orgamiryazdani@gmail.com>
2025-07-02 01:04:41 +0330

    refactor: remove old code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,3 @@
-import { getTranslations } from 'next-intl/server';
-
-export default async function Home() {
-  const t = await getTranslations('HomePage');
-
-  return (
-    <div>
-      <h1>{t('title')}</h1>
-      <h2>{t('content')}</h2>
-    </div>
-  );
+export default function Home() {
+  return <div></div>;
 }
